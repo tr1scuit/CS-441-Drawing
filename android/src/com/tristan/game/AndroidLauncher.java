cc51383f2024c8237d90ffe2f5c628c24b9d7755
Tristan Awayan <jawayan1@binghamton.edu>
2021-03-03 11:28:24 -0500

    Initialize LibGDX and Gradle
    Initial Android Studio project

---
+++ android/src/com/tristan/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tristan.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tristan.game.TristanDrawingGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TristanDrawingGame(), config);
+	}
+}
